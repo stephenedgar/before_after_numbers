af3153310c14b651853963fad723169e44ddf9d8
Stephen Edgar <stephen@1partcarbon.co.uk>
2015-11-29 23:16:58 +0000

    Add number and some styles

--- www/js/game.js
+++ www/js/game.js
@@ -2,6 +2,13 @@ var gameModule = angular.module('game', []);
 
 gameModule.controller('GameController', ['$scope', '$state',
   function($scope, $state){
+    $scope.middleNumber = 16;
+    $scope.beforeChoiceOne = 11;
+    $scope.beforeChoiceTwo = 15;
+    $scope.beforeChoiceThree = 2;
+    $scope.afterChoiceOne = 17;
+    $scope.afterChoiceTwo = 29;
+    $scope.afterChoiceThree = 6;
 
   }
 ]);
