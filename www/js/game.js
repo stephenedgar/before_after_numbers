65622d5f17c74f241c6d0b1c0106d8a2fffb1684
Stephen Edgar <stephen@1partcarbon.co.uk>
2015-12-01 00:05:46 +0000

    Trying to lay out grid and basic game working

--- www/js/game.js
+++ www/js/game.js
@@ -9,6 +9,21 @@ gameModule.controller('GameController', ['$scope', '$state',
     $scope.afterChoiceOne = 17;
     $scope.afterChoiceTwo = 29;
     $scope.afterChoiceThree = 6;
-
+    $scope.beforeSelected = function(number) {
+      if(number == ($scope.middleNumber - 1)) {
+        alert("correct");
+      }
+      else {
+        alert("try again");
+      }
+    }
+    $scope.afterSelected = function(number) {
+      if(number == ($scope.middleNumber + 1)) {
+        alert("correct");
+      }
+      else {
+        alert("try again");
+      }
+    }
   }
 ]);
